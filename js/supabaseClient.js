1c9aa2568af008261f1f17a506afc2ad5355d0c0
appvastbiz-prog <appvastbiz@gmail.com>
2026-03-05 07:20:57 +0800

    Create supabaseClient.js

---
+++ js/supabaseClient.js
@@ -0,0 +1,7 @@
+const supabaseUrl = "https://vhfdtequcekmmwoiikhm.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZoZmR0ZXF1Y2VrbW13b2lpa2htIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg2NzI3NDEsImV4cCI6MjA4NDI0ODc0MX0.FWGLnZ4w0BfNwWYb5_MM0n7v2iyojZaRB14BbB7pdKM";
+
+const supabase = window.supabase.createClient(
+supabaseUrl,
+supabaseKey
+);
