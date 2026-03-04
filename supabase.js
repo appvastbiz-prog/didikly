bf52d6fcc84d8a77700f78f227d24432fce45b56
appvastbiz-prog <appvastbiz@gmail.com>
2026-03-04 08:19:03 +0800

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,7 @@
+<script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2"></script>
+<script>
+  const supabaseUrl = "https://vhfdtequcekmmwoiikhm.supabase.co";
+  const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZoZmR0ZXF1Y2VrbW13b2lpa2htIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg2NzI3NDEsImV4cCI6MjA4NDI0ODc0MX0.FWGLnZ4w0BfNwWYb5_MM0n7v2iyojZaRB14BbB7pdKM";
+
+  const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
+</script>
